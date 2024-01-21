3a3e95ed565d3713549d816b18e512178c394471
Micki Balder <mfbalder@gmail.com>
2024-01-21 15:12:19 -0700

    Add class to swap a string's case, and specs

---
+++ swap_case.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+###
+# From scratch, write a ruby object that swaps case.
+# Lowercase should be turned into uppercase and vice-versa. “Hello” -> “hELLO.”
+# Spaces and numbers should be preserved, but any special characters should raise errors.
+# Write specs for each using RSpec’s single-line “it” syntax.
+###
+class SwapCase
+  def self.execute(input)
+    validate_input(input)
+
+    input.chars.each_with_object('') do |char, output|
+      output << char.swapcase
+    end
+  end
+
+  def self.validate_input(input)
+    raise 'Input cannot contain special characters' if input.match(/[^a-zA-Z0-9\s]/)
+  end
+end
---
+++ swap_case_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/BlockLength
+
+require_relative './swap_case'
+RSpec.describe SwapCase do
+  subject { SwapCase.execute(input) }
+
+  context 'with special characters in input' do
+    let(:input) { 'hello.' }
+    it { expect { subject }.to raise_error }
+  end
+
+  context 'With valid input' do
+    describe 'with lowercase letters' do
+      let(:input) { 'hello' }
+      it { expect(subject).to eq('HELLO') }
+    end
+
+    describe 'with uppercase letters' do
+      let(:input) { 'HELLO' }
+      it { expect(subject).to eq('hello') }
+    end
+
+    describe 'with upper and lowecase letters' do
+      let(:input) { 'heLLo' }
+      it { expect(subject).to eq('HEllO') }
+    end
+
+    describe 'with a space' do
+      let(:input) { 'hello hello' }
+      it { expect(subject).to eq('HELLO HELLO') }
+    end
+
+    describe 'with numbers' do
+      let(:input) { 'h3llo' }
+      it { expect(subject).to eq('H3LLO') }
+    end
+  end
+end
+
+# rubocop:enable Metrics/BlockLength
